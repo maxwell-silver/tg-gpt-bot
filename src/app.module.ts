1563d4f2b99333348d7671eaa179328cf7775478
Ilya Rodin <i1.rodin@yandex.ru>
2023-07-25 20:15:18 +0300

    format code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ChatgptModule } from './chatgpt/chatgpt.module'
+import { TelegramModule } from './telegram/telegram.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [ChatgptModule, TelegramModule],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
